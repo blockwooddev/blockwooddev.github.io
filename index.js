7afede9e33b1f00769c682041f307803c53f05c5
Barry <barrylockwood.swdev@gmail.com>
2017-10-07 20:41:32 -0700

    More changes to get started

--- index.js
+++ index.js
@@ -1,17 +1,95 @@
-$('[data-toggle=collapse]').click(function(){
-	
-  	// toggle icon
-  	$(this).find("i").toggleClass("icon-angle-right icon-angle-down");
+
+var portfolioApp = angular.module('portfolioApp', ['ui.router']);
+
+portfolioApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+          url: '/about',
+          views: {
+            '': {
+              templateUrl: 'about.html'
+            },
+            'columnOne@about': {
+              template: 'look I am a column'
+            },
+            'columnTwo@about': {
+              templateUrl: 'table-data.html',
+              controller: 'portfolioController'
+            }
+          }      
+        })
+        
+        .state('home.list', {
+          url: '/list',
+          templateUrl: 'home-list.html',
+          controller: function($scope) {
+              $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+          }
+        })
   
+        // nested list with just some random string data
+        .state('home.paragraph', {
+          url: '/paragraph',
+          template: 'I could sure use a drink right now.'
+        });
+
 });
 
-$('.collapse').on('show', function (e) {
+
+
+portfolioApp.controller('portfolioController', function portfolioController($scope) {
+  $scope.message = 'test';
   
-  	// hide open menus
-  	$('.collapse').each(function(){
-      if ($(this).hasClass('in')) {
-          $(this).collapse('toggle');
-      }
-    });
-  
-})
+  $scope.scotches = [
+    {
+      name: 'Macallan 12',
+      price: 50
+    },
+    {
+      name: 'Chivas Regal Royal Salute',
+      price: 100
+    },
+    {
+      name: 'Glenfiddich 1937',
+      price: 2000
+    },
+  ];
+});
+
+
+
+
+
+
+
+
+
+
+//$('[data-toggle=collapse]').click(function(){
+//	
+//  	// toggle icon
+//  	$(this).find("i").toggleClass("icon-angle-right icon-angle-down");
+//  
+//});
+//
+//$('.collapse').on('show', function (e) {
+//  
+//  	// hide open menus
+//  	$('.collapse').each(function(){
+//      if ($(this).hasClass('in')) {
+//          $(this).collapse('toggle');
+//      }
+//    });
+//  
+//})
